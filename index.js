6fd8941ee791af81cd9a64734ea1354e3758d764
GaziFayaz <gazifayaz.16694@gmail.com>
2024-04-28 11:10:39 +0600

    get users, get specific user, post user, get tourist spots, get specific tourist spot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -23,7 +23,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
+    const userCollection = client.db("B9A10-tourism-management").collection("users");
     const countryCollection = client.db("B9A10-tourism-management").collection("countries");
+    const touristSpotCollection = client.db("B9A10-tourism-management").collection("touristSpots")
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get("/user/:uid", async (req, res) => {
+      const id = req.params.id;
+      const query = {uid: uid}
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post("/user", async (req, res) => {
+      const newUser = req.body
+      console.log(newUser)
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    app.get("/tourist-spots", async (req, res) => {
+      const cursor = touristSpotCollection.find()
+      const result = await cursor.toArray()
+      res.send()
+    })
+
+    app.get("/tourist-spot/:id", async (req, res) => {
+      const _id = req.params.id;
+      const query = {_id: new ObjectId(_id)}
+      const result = await touristSpotCollection.findOne(query)
+      res.send(result)
+    })
 
     app.get("/countries", async (req, res) => {
       const cursor = countryCollection.find();
